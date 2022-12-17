d8894a268b6d2eafba8b965380f9175a21d38881
Agabi Seidaliyev <quhanWorkEmail@gmail.com>
2022-12-17 16:01:30 +0600

    Add lesson5 @RequestParam

---
+++ SpringMVC/Lesson5 Model/src/main/java/kz/quhan/springcourse/controller/FirstController.java
@@ -0,0 +1,50 @@
+package kz.quhan.springcourse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message", "hello " + name + " " + surname);
+
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbyePage() {
+        return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) int a,
+                             @RequestParam(value = "b", required = false) int b,
+                             @RequestParam(value = "action", required = false) String action,
+                             Model model) {
+        switch (action) {
+            case "plus":
+                model.addAttribute("result", a + b);
+                break;
+            case "minus":
+                model.addAttribute("result", a - b);
+                break;
+            case "divide":
+                model.addAttribute("result", a / b);
+                break;
+            case "multiply":
+                model.addAttribute("result", a * b);
+                break;
+        }
+
+        return "first/calculator";
+    }
+}
